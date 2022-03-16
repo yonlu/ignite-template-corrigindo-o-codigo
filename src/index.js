62fefb481bc9af81d631df310d0769cf8dc32aa2
yonlu <lss.emailbox@gmail.com>
2022-03-16 13:00:06 -0400

    feat: finish challange

--- src/index.js
+++ src/index.js
@@ -23,6 +23,8 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
+  repositories.push(repository);
+
   return response.json(repository);
 });
 
@@ -30,10 +32,16 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
+  }
+
+  // prevent manually updating the like counter
+  if (updatedRepository.likes !== repositories[repositoryIndex].likes) {
+    updatedRepository.likes = repositories[repositoryIndex].likes;
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
@@ -48,7 +56,7 @@ app.delete("/repositories/:id", (request, response) => {
 
   repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +74,10 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  const repository = repositories[repositoryIndex];
+  repository.likes = repository.likes + 1;
 
-  return response.json('likes');
+  return response.json(repository);
 });
 
 module.exports = app;
